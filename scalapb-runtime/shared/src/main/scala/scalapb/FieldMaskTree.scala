0af3c868a2144f9a21c3a4d4fb21c2bdcc7b53c9
Yaroslav Krukovsky <iaroslavk@wix.com>
2018-07-04 13:11:03 +0300

    working on FieldMaskTree

--- scalapb-runtime/shared/src/main/scala/scalapb/FieldMaskTree.scala
+++ scalapb-runtime/shared/src/main/scala/scalapb/FieldMaskTree.scala
@@ -1,29 +1,58 @@
 package scalapb
 
 import com.google.protobuf.field_mask.FieldMask
-
+import FieldMaskUtil._
 import scala.collection.mutable
 
-protected case class Node(children: mutable.TreeMap[String,Node])
+protected case class Node(children: mutable.TreeMap[String, Node] = mutable.TreeMap.empty)
 
 case class FieldMaskTree(root: Node = Node(mutable.TreeMap.empty[String, Node])) extends Traversable[String] {
+  private val FIELD_PATH_SEPARATOR_REGEX = "\\."
 
   def this(fieldMask: FieldMask) =
     this(FieldMaskTree.mergeFromFieldMask(FieldMaskTree(), fieldMask).root)
 
-  def toFieldMask: FieldMask = ???
+  def toFieldMask: FieldMask = FieldMask(getFieldPaths(root, "", List.empty))
 
-  def mergeFromFieldMask(fieldMask: FieldMask): FieldMaskTree ={
+  def mergeFromFieldMask(fieldMask: FieldMask): FieldMaskTree = {
     FieldMaskTree.mergeFromFieldMask(this, fieldMask)
   }
 
   def intersect(anotherTree: FieldMaskTree): FieldMaskTree = ???
 
-  def addFieldPath(path: String): FieldMaskTree  = ???
+  def addFieldPath(path: String): FieldMaskTree = {
+    val pathParts = path.split(FIELD_PATH_SEPARATOR_REGEX).toList
+    val newTree = this
+
+    def innerAddFieldPath(node: Node, pathParts: List[String]): Unit = pathParts match {
+      case Nil => node.children.clear()
+      case x :: xs => node.children.get(x) match {
+        case Some(n) => innerAddFieldPath(n, xs)
+        case None =>
+          val nextNode = Node()
+          node.children.put(x, nextNode)
+          innerAddFieldPath(nextNode, xs)
+      }
+    }
+
+    innerAddFieldPath(newTree.root, pathParts)
+    newTree
+  }
+
+  private def getFieldPaths(node: Node, path: String, paths: List[String]): List[String] = node.children match {
+    case tree if tree.isEmpty => path :: paths
+    case tree => tree.map { case (name, value) =>
+      getFieldPaths(
+        value,
+        if (path.isEmpty) name else s"$path.$name",
+        paths
+      )
+    }.reduce(_ ::: _)
+  }
 
   override def foreach[U](f: String => U): Unit = ???
 
-  override def toString(): String = ???
+  override def toString(): String = toFieldMask.toSingleString
 }
 
 object FieldMaskTree {
--- scalapb-runtime/shared/src/test/scala/scalapb/FieldMaskTreeSpec.scala
+++ scalapb-runtime/shared/src/test/scala/scalapb/FieldMaskTreeSpec.scala
@@ -3,43 +3,123 @@ package scalapb
 import com.google.protobuf.field_mask.FieldMask
 import utest._
 
+import scala.collection.mutable
+
 object FieldMaskTreeSpec extends TestSuite {
   val tests = Tests {
     val someFieldMask = FieldMask(Seq(""))
+    val emptyTree = FieldMaskTree()
+    val singleNodeTree = FieldMaskTree(
+      Node(
+        mutable.TreeMap("foo" ->
+          Node(mutable.TreeMap("baz" ->
+            Node(mutable.TreeMap("bar" -> Node(mutable.TreeMap.empty))))
+          )
+        )
+      )
+    )
+    val multipleNodesTree = FieldMaskTree(
+      Node(
+        mutable.TreeMap("foo" ->
+          Node(mutable.TreeMap("baz" ->
+            Node(mutable.TreeMap("bar" -> Node(mutable.TreeMap.empty))))
+          ),
+          "a" ->
+            Node(mutable.TreeMap("b" ->
+              Node(mutable.TreeMap("c" -> Node(mutable.TreeMap.empty))))
+            )
+        )
+      )
+    )
 
     "addFieldPath should" - {
       "return same tree when path is empty" - {
-
+        assert(
+          emptyTree.addFieldPath("") == emptyTree
+        )
       }
 
-      "return tree with added path" - {
+      "return tree with single added path" - {
+        assert(
+          emptyTree.addFieldPath("foo.baz.bar") == singleNodeTree
+        )
+      }
 
+      "return tree with multiple added paths" - {
+        assert(
+          singleNodeTree.addFieldPath("a.b.c") == multipleNodesTree
+
+        )
       }
 
       "return tree with simplified path" - {
-
+        assert(
+          singleNodeTree.addFieldPath("foo.baz") == FieldMaskTree(
+            Node(
+              mutable.TreeMap("foo" ->
+                Node(mutable.TreeMap("baz" -> Node(mutable.TreeMap.empty)))
+              )
+            )
+          )
+        )
       }
     }
 
     "mergeFromFieldMask should" - {
 
-      "return empty tree when empty field mask provided" - {
-
+      "return the same tree when empty field mask provided" - {
+        assert(
+          emptyTree.mergeFromFieldMask(FieldMask(Seq.empty)) == emptyTree
+        )
       }
 
-      "return tree from field mask" - {
+      "return single node tree from field mask applied to empty tree" - {
+        assert(
+          emptyTree.mergeFromFieldMask(FieldMask(Seq("foo.baz.bar"))) == singleNodeTree
+        )
+      }
 
+      "return multiple node tree from single path field mask applied to single node tree" - {
+        assert(
+          singleNodeTree.mergeFromFieldMask(FieldMask(Seq("a.b.c"))) == multipleNodesTree
+        )
+      }
+
+      "return multiple node tree from multiple path field mask applied to single node tree" - {
+        assert(
+          singleNodeTree.mergeFromFieldMask(FieldMask(Seq("a.b.c", "x.y.z"))) == FieldMaskTree(
+            Node(
+              mutable.TreeMap("foo" ->
+                Node(mutable.TreeMap("baz" ->
+                  Node(mutable.TreeMap("bar" -> Node(mutable.TreeMap.empty))))
+                ),
+                "a" ->
+                  Node(mutable.TreeMap("b" ->
+                    Node(mutable.TreeMap("c" -> Node(mutable.TreeMap.empty))))
+                  ),
+                "x" ->
+                  Node(mutable.TreeMap("y" ->
+                    Node(mutable.TreeMap("z" -> Node(mutable.TreeMap.empty))))
+                  )
+              )
+            )
+          )
+        )
       }
     }
 
     "toFieldMask should" - {
 
       "convert empty tree to empty field mask" - {
-
+        emptyTree.toFieldMask == FieldMask(Seq.empty)
       }
 
-      "convert tree to field mask" - {
+      "convert single node tree to field mask" - {
+        singleNodeTree.toFieldMask == FieldMask(Seq("foo.baz.bar"))
+      }
 
+      "convert multiple nodes tree to field mask" - {
+        multipleNodesTree.toFieldMask == FieldMask(Seq("foo.baz.bar", "a.b.c"))
       }
     }
 
@@ -62,10 +142,6 @@ object FieldMaskTreeSpec extends TestSuite {
       "traverse tree" - {
 
       }
-    }
-
-    "toString should return string interpretation" - {
-
     }
   }
 }
