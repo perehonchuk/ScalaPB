31a695abf9e0103c71f173ba11c7dfc8f8ed3e18
Yaroslav Krukovsky <iaroslavk@wix.com>
2018-07-10 14:05:14 +0300

    Merge with empty PMessage testing

--- e2e/src/test/scala/FieldMaskUtilSpec.scala
+++ e2e/src/test/scala/FieldMaskUtilSpec.scala
@@ -3,7 +3,7 @@ import protobuf_unittest.unittest._
 import com.google.protobuf.field_mask.FieldMask
 import org.scalatest.matchers._
 import scalapb.GeneratedMessage
-import scalapb.descriptors.PMessage
+import TestAllTypes.NestedMessage
 
 class FieldMaskUtilSpec extends FlatSpec with MustMatchers {
   "NestedTestAllTypes" should "be valid" in {
@@ -25,28 +25,100 @@ class FieldMaskUtilSpec extends FlatSpec with MustMatchers {
     NestedTestAllTypes.isValid("payload.optional_int32.bb") must be(false)
   }
 
-  "TestAllTypes" should "be able to merge payload.optional_int32" in new ctx {
-    val someFieldMask = FieldMask(Seq("payload.optional_int32"))
-    val newMessage = NestedTestAllTypes.messageReads.read(scalapb.FieldMaskUtil.merge(someFieldMask, source.toPMessage))
-
-    val expectedMessage = NestedTestAllTypes(
+  "Merge with empty PMessage" should "be able to merge payload.optional_int32" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("payload.optional_int32"))
+    override def expectedMessage = NestedTestAllTypes(
       payload = Some(
         TestAllTypes(
           optionalInt32 = Some(1234)
         )
       )
     )
-
-    newMessage must beEqualToMessage(expectedMessage)
   }
 
-  it should "be able to merge smth else " in new ctx {
-
+  it should "be able to merge payload.optional_nested_message" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("payload.optional_nested_message"))
+    override def expectedMessage = NestedTestAllTypes(
+      payload = Some(
+        TestAllTypes(
+          optionalNestedMessage = Some(NestedMessage(Some(5678)))
+        )
+      )
+    )
   }
 
-  trait ctx {
-    import TestAllTypes.NestedMessage
+  it should "be able to merge payload.repeated_int32" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("payload.repeated_int32"))
+    override def expectedMessage = NestedTestAllTypes(
+      payload = Some(
+        TestAllTypes(
+          repeatedInt32 = Seq(4321)
+        )
+      )
+    )
+  }
 
+  it should "be able to merge payload.repeated_nested_message" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("payload.repeated_nested_message"))
+    override def expectedMessage = NestedTestAllTypes(
+      payload = Some(
+        TestAllTypes(
+          repeatedNestedMessage = Seq(NestedMessage(Some(8765)))
+        )
+      )
+    )
+  }
+
+  it should "be able to merge child.payload.optional_int32" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("child.payload.optional_int32"))
+    override def expectedMessage = NestedTestAllTypes(
+      child = Some(NestedTestAllTypes(
+        payload = Some(TestAllTypes(
+          optionalInt32 = Some(1234)
+        ))
+      ))
+    )
+  }
+
+  it should "be able to merge child.payload.optional_nested_message" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("child.payload.optional_nested_message"))
+    override def expectedMessage = NestedTestAllTypes(
+      child = Some(NestedTestAllTypes(
+        payload = Some(TestAllTypes(
+          optionalNestedMessage = Some(NestedMessage(Some(5678)))
+        ))
+      ))
+    )
+  }
+
+  it should "be able to merge child.payload.repeated_int32" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("child.payload.repeated_int32"))
+    override def expectedMessage = NestedTestAllTypes(
+      child = Some(NestedTestAllTypes(
+        payload = Some(TestAllTypes(
+          repeatedInt32 = Seq(4321)
+        ))
+      ))
+    )
+  }
+
+  it should "be able to merge child.payload.repeated_nested_message" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("child.payload.repeated_nested_message"))
+    override def expectedMessage = NestedTestAllTypes(
+      child = Some(NestedTestAllTypes(
+        payload = Some(TestAllTypes(
+          repeatedNestedMessage = Seq(NestedMessage(Some(8765)))
+        ))
+      ))
+    )
+  }
+
+  it should "be able to merge all fields" in new emptyPMessageCtx {
+    override def someFieldMask = FieldMask(Seq("child", "payload"))
+    override def expectedMessage = source
+  }
+
+  trait emptyPMessageCtx extends baseCtx {
     val value = TestAllTypes(
       optionalInt32 = Some(1234),
       optionalNestedMessage = Some(NestedMessage(Some(5678))),
@@ -59,7 +131,6 @@ class FieldMaskUtilSpec extends FlatSpec with MustMatchers {
         payload = Some(value)
       ))
     )
-
     // Now we have a message source with the following structure:
     //   [root] -+- payload -+- optional_int32
     //           |           +- optional_nested_message
@@ -71,6 +142,15 @@ class FieldMaskUtilSpec extends FlatSpec with MustMatchers {
     //                                 +- repeated_int32
     //                                 +- repeated_nested_message
 
+
+    def someFieldMask: FieldMask
+    def expectedMessage: GeneratedMessage
+    val newMessage = NestedTestAllTypes.messageReads.read(scalapb.FieldMaskUtil.merge(someFieldMask, source.toPMessage))
+
+    newMessage must beEqualToMessage(expectedMessage)
+  }
+
+  trait baseCtx {
     def beEqualToMessage(right: GeneratedMessage) = new Matcher[GeneratedMessage] {
       def apply(left: GeneratedMessage) = org.scalatest.matchers.MatchResult(
         left.companion.messageReads.read(left.toPMessage) == right,
